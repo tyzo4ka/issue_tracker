603a4d1d2e8af6689ab189f40eb83fbc1fe5af65
tyzo4ka <tyzo4ka@gmail.com>
2019-12-05 11:20:31 +0600

    Added nested Serializer for Project model to view all Issues

--- source/api/serializers.py
+++ source/api/serializers.py
@@ -2,15 +2,20 @@ from webapp.models import Project, Issue
 from rest_framework import serializers
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = ('id', 'name', 'description', 'created_date', 'updated_date', 'status')
-
-
 class IssueSerializer(serializers.ModelSerializer):
+    created_date = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
     class Meta:
         model = Issue
         fields = ('id', 'summary', 'description', 'status', 'type', 'project', 'created_date', 'updated_at',
                   'created_by', 'assigned_to')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    issues = IssueSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = ('id', 'name', 'description', 'created_date', 'updated_date', 'status', 'issues')
 
