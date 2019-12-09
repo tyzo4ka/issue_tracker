cd2a038ff9e8d404d434b275dc33f6bde45dce9a
tyzo4ka <tyzo4ka@gmail.com>
2019-12-09 19:17:04 +0600

    typo fixed

--- source/webapp/static/js/HW70_ajax_requests.js
+++ source/webapp/static/js/HW70_ajax_requests.js
@@ -42,7 +42,7 @@ function createIssue(){
         url : 'http://localhost:9000/api/issues/',
         method: 'post',
         headers : {"Authorization" : "Token bc4ac83cfbaae30c5ae1e99f3c8a451aa37d087a"},
-        data: JSON.stringify({summary: "Test", desctiption: "Test", status:1, type: 1, project: 1 , created_by: 1,
+        data: JSON.stringify({summary: "Test", description: "Test", status:1, type: 1, project: 1 , created_by: 1,
         assigned_to: 1}),
         contentType:"application/json",
         dataType : "json",
